fa5a8b956f729065985ad69931840afee136cdc5
Felipe T Tristao <f.torrestristao@yahoo.com.br>
2017-09-23 13:06:05 -0300

    create and update chatrooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
+
   get 'login', to: "sessions#new"
   post '/login', to: "sessions#create"
   #get 'sessions/destroy'
 
   resources :users, only: [:new, :create]
-
+  resources :chatrooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
